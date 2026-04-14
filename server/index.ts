c9060b4f647d6ddd72c06d3f27276f6e56dc11fc
Maarten de Jager <maarten.dejager@visma.com>
2026-04-14 14:54:32 +0200

    small fixes

--- server/index.ts
+++ server/index.ts
@@ -39,6 +39,9 @@ app.use(cors({
     console.warn("CORS blocked origin:", origin);
     callback(new Error("Not allowed by CORS"));
   },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 
